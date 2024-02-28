1649f4480fe6b3488232f164706853e9b73e3646
jjmaitan <jjmaitan@colostate.edu>
2024-02-28 12:28:14 -0700

    Passed the HW2 test server

--- src/main/java/csx55/threads/MatrixThreads.java
+++ src/main/java/csx55/threads/MatrixThreads.java
@@ -6,13 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MatrixThreads {
 
-    private final static ReentrantLock mainThreadLock = new ReentrantLock();
     public static void main(String[] args) {
-
-        // final int THREAD_POOL_SIZE = 8;
-        // final int MATRIX_DIMENSIONS = 3;
-        // final int SEED = 31459;
-
         if (args.length < 3) {
             System.err.println("Invalid # of arguments");
             System.exit(1);
@@ -28,9 +22,7 @@ public class MatrixThreads {
         } // End if statement
 
         ThreadPool pool = new ThreadPool(THREAD_POOL_SIZE);
-        // pool.startAllThreads();
-        // ArrayList<Matrix> matrices = new ArrayList<>();
-        
+    
         System.out.println("Dimensionality of the square matrices is: " + MATRIX_DIMENSIONS);
         System.out.println("The thread pool has been initialized to: " + THREAD_POOL_SIZE);
         System.out.println();
@@ -46,62 +38,32 @@ public class MatrixThreads {
         Matrix c = new Matrix('C', MATRIX_DIMENSIONS, numberGenerator);
         Matrix d = new Matrix('D', MATRIX_DIMENSIONS, numberGenerator);
 
-        // matrices.add(a);
-        // matrices.add(b);
-        // matrices.add(c);
-        // matrices.add(d);
-
-        // for (Matrix m : matrices) {
-             
-        //     int[][] arr = new int[MATRIX_DIMENSIONS][MATRIX_DIMENSIONS];
-
-        //     for (int column = 0; column < arr.length; ++column) {
-        //         for (int row = 0; row < arr.length; ++row) {
-        //             // int randomValue = numberGenerator.nextInt(10) + 1;
-        //             int randomValue = 1000 - numberGenerator.nextInt(2000);
-        //             arr[column][row] = randomValue;
-        //         } // End nested for loop
-        //     } // End for loop
-
-        //     m.setData(arr);
-        // } // End for each loop
-
-        // for (Matrix m : matrices) {
-        //     System.out.println("Sum of the elements in input matrix " + m.getName() + " = " + m.sumOfMatrixElements(m.data, MATRIX_DIMENSIONS));
-        // } // End for each loop
-
         System.out.println();
 
         Matrix x = new Matrix('X', MATRIX_DIMENSIONS);
         Matrix y = new Matrix('Y', MATRIX_DIMENSIONS);
         Matrix z = new Matrix('Z', MATRIX_DIMENSIONS);
 
-        mainThreadLock.lock();
-        // System.out.println("Is main thread locked? " + mainThreadLock.isLocked());
-        try {
-            // mainThreadLock.wait();
-            System.out.println("Calculating Matrix X");
-            // System.out.println(a.toString());
-            // System.out.println(b.toString());
-            x.data = x.multiplyMatrices(a, b, MATRIX_DIMENSIONS, pool);
-            // System.out.println(x.toString());
+        
+        // mainThreadLock.wait();
+        System.out.println("Calculating Matrix X");
+        // System.out.println(a.toString());
+        // System.out.println(b.toString());
+        x.data = x.multiplyMatrices(a, b, MATRIX_DIMENSIONS, pool);
+        // System.out.println(x.toString());
 
-            System.out.println("Calculating Matrix Y");
-            // System.out.println(c.toString());
-            // System.out.println(d.toString());
-            y.data = y.multiplyMatrices(c, d, MATRIX_DIMENSIONS, pool);
-            // System.out.println(y.toString());
+        System.out.println("Calculating Matrix Y");
+        // System.out.println(c.toString());
+        // System.out.println(d.toString());
+        y.data = y.multiplyMatrices(c, d, MATRIX_DIMENSIONS, pool);
+        // System.out.println(y.toString());
 
-            System.out.println("Calculating Matrix Z");
-            // System.out.println(x.toString());
-            // System.out.println(y.toString());
-            z.data = z.multiplyMatrices(x, y, MATRIX_DIMENSIONS, pool);
-            // System.out.println(z.toString());
-        } catch (Exception err) {
-            System.err.println(err.getMessage());
-        } finally {
-            mainThreadLock.unlock();
-        } // End try-catch statement
+        System.out.println("Calculating Matrix Z");
+        // System.out.println(x.toString());
+        // System.out.println(y.toString());
+        z.data = z.multiplyMatrices(x, y, MATRIX_DIMENSIONS, pool);
+        // System.out.println(z.toString());
+        
         
         double cumulativeTime = x.getTime() + y.getTime() + z.getTime();
        
--- src/main/java/csx55/threads/ThreadPool.java
+++ src/main/java/csx55/threads/ThreadPool.java
@@ -1,28 +1,24 @@
 package csx55.threads;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadPool {
 
     private Thread[] threads; /* used to hold our threads */
-    // private volatile String name;
-
-    // TODO: Figure out a better way to get jobs and return them?
-    private static volatile LinkedBlockingQueue<Job> jobQueue = new LinkedBlockingQueue<>();
-    
-    // private static volatile boolean threadPoolLock = true;
-    // private static volatile boolean start = false;
+    private static volatile ConcurrentLinkedQueue<Job> jobQueue = new ConcurrentLinkedQueue<>();
+    // private static volatile LinkedBlockingQueue<Job> jobQueue = new LinkedBlockingQueue<>();
 
     public ThreadPool(int size) {
         threads = new Thread[size];
 
         for (int i = 0; i < threads.length; ++i) {
-            threads[i] = new Thread(this::workerThread);
+            threads[i] = new Thread(this::workerThread, "" + i);
             threads[i].setDaemon(true);
             threads[i].start();
-        }
-    }
-
+        } // End for loop
+    } // End ThreadPool(int) constructor
+ 
     public void addJob(Job newJob) {
         jobQueue.add(newJob);
     } // End addJob(j) method
@@ -35,32 +31,39 @@ public class ThreadPool {
         while (true) {
             Job j;
 
-            try {
-                j = jobQueue.take();
-            } catch (InterruptedException err) {
-                break;
-            }
+            // try {
+            //     // System.out.println(Thread.currentThread().getName());
+            //     j = jobQueue.take();
+            // } catch (InterruptedException err) {
+            //     break;
+            // }
             
-            
-            int product = 0;
-            int[][] m1 = j.getMatrixOne();
-            int[][] m2 = j.getMatrixTwo();
-            int[][] productMatrix = j.getProductMatrix();
+            j = jobQueue.poll();
 
-            int[] m1Row = m1[j.getRow()];
-            int[] m2Col = m2[j.getCol()];
+            if (j != null) {
+                int product = 0;
+                int[][] m1 = j.getMatrixOne();
+                int[][] m2 = j.getMatrixTwo();
+                int[][] productMatrix = j.getProductMatrix();
+    
+                int[] m1Row = m1[j.getRow()];
+                int[] m2Col = m2[j.getCol()];
+    
+                for (int pos = 0; pos < m1.length; ++pos) {
+                    // This works with a second matrix that is not transposed
+                    // product += m1[j.getRow()][pos] * m2[pos][j.getCol()];
+    
+                    // Case where m2 is transposed
+                    product += m1Row[pos] * m2Col[pos];
+                } // End outside for loop
+    
+                productMatrix[j.getRow()][j.getCol()] = product; 
+            } else {
+                Thread.onSpinWait();
+            } // End if-else statement
 
-            for (int pos = 0; pos < m1.length; ++pos) {
-                // This works with a second matrix that is not transposed
-                // product += m1[j.getRow()][pos] * m2[pos][j.getCol()];
-
-                // Case where m2 is transposed
-                product += m1Row[pos] * m2Col[pos];
-            } // End outside for loop
-
-            productMatrix[j.getRow()][j.getCol()] = product;
-            
         } // End while loop
+
     } // End workerThread() method
 
 } // End ThreadPool class
